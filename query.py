a6ce5418014e699500f8aa4c8413e75ca723127b
deepshig <deepshi1104@gmail.com>
2020-02-24 18:14:11 +0100

    Change column names to have underscores instead of hyphens

--- query.py
+++ query.py
@@ -1,43 +1,43 @@
 import random as rd
 
 attributes = {
-    1: "Class Name",
-    2: "handicapped-infants",
-    3: "water-project-cost-sharing",
-    4: "adoption-of-the-budget-resolution",
-    5: "physician-fee-freeze",
-    6: "el-salvador-aid",
-    7: "religious-groups-in-schools",
-    8: "anti-satellite-test-ban",
-    9: "aid-to-nicaraguan-contras",
-    10: "mx-missile",
+    1: "class_name",
+    2: "handicapped_infants",
+    3: "water_project_cost_sharing",
+    4: "adoption_of_the_budget_resolution",
+    5: "physician_fee_freeze",
+    6: "el_salvador_aid",
+    7: "religious_groups_in_schools",
+    8: "anti_satellite_test_ban",
+    9: "aid_to_nicaraguan_contras",
+    10: "mx_missile",
     11: "immigration",
-    12: "synfuels-corporation-cutback",
-    13: "education-spending",
-    14: "superfund-right-to-sue",
+    12: "synfuels_corporation_cutback",
+    13: "education_spending",
+    14: "superfund_right_to_sue",
     15: "crime",
-    16: "duty-free-exports",
-    17: "export-administration-act-south-africa"
+    16: "duty_free_exports",
+    17: "export_administration_act_south_africa"
 }
 
 attributeValues = {
-    "Class Name": ["democrat", "republican"],
-    "handicapped-infants": ["y", "n", "?"],
-    "water-project-cost-sharing": ["y", "n", "?"],
-    "adoption-of-the-budget-resolution": ["y", "n", "?"],
-    "physician-fee-freeze": ["y", "n", "?"],
-    "el-salvador-aid": ["y", "n", "?"],
-    "religious-groups-in-schools": ["y", "n", "?"],
-    "anti-satellite-test-ban": ["y", "n", "?"],
-    "aid-to-nicaraguan-contras": ["y", "n", "?"],
-    "mx-missile": ["y", "n", "?"],
+    "class_name": ["democrat", "republican"],
+    "handicapped_infants": ["y", "n", "?"],
+    "water_project_cost_sharing": ["y", "n", "?"],
+    "adoption_of_the_budget_resolution": ["y", "n", "?"],
+    "physician_fee_freeze": ["y", "n", "?"],
+    "el_salvador_aid": ["y", "n", "?"],
+    "religious_groups_in_schools": ["y", "n", "?"],
+    "anti_satellite_test_ban": ["y", "n", "?"],
+    "aid_to_nicaraguan_contras": ["y", "n", "?"],
+    "mx_missile": ["y", "n", "?"],
     "immigration": ["y", "n", "?"],
-    "synfuels-corporation-cutback": ["y", "n", "?"],
-    "education-spending": ["y", "n", "?"],
-    "superfund-right-to-sue": ["y", "n", "?"],
+    "synfuels_corporation_cutback": ["y", "n", "?"],
+    "education_spending": ["y", "n", "?"],
+    "superfund_right_to_sue": ["y", "n", "?"],
     "crime": ["y", "n", "?"],
-    "duty-free-exports": ["y", "n", "?"],
-    "export-administration-act-south-africa": ["y", "n", "?"]
+    "duty_free_exports": ["y", "n", "?"],
+    "export_administration_act_south_africa": ["y", "n", "?"]
 }
 
 
@@ -51,7 +51,7 @@ def get_random_conditions():
         condition_name = attributes[condition_index]
 
         possible_values = attributeValues[condition_name]
-        value_index = rd.randint(0, (len(possible_values)-1))
+        value_index = rd.randint(0, (len(possible_values) - 1))
         value = possible_values[value_index]
 
         conditions[condition_name] = value
@@ -67,10 +67,8 @@ def generate_query():
     for name in condtions:
         query = query + name + "='" + condtions[name] + "'"
 
-        if i < len(condtions)-1:
+        if i < len(condtions) - 1:
             query = query + " AND "
-        else:
-            query = query + ";"
 
         i = i+1
 
