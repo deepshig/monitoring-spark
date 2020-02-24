f16a043f9043a99db0d314ffb32c484cba09e006
deepshig <deepshi1104@gmail.com>
2020-02-24 18:14:58 +0100

    Add logic to read voting_records from csv and search records

--- read_data.py
+++ read_data.py
@@ -8,16 +8,30 @@ sc = SparkContext()
 spark = SparkSession.builder.getOrCreate()
 
 schema = StructType([
-    StructField("ID", IntegerType()),
-    StructField("Country", StringType()),
-    StructField("Capital", StringType())
+    StructField("class_name", StringType()),
+    StructField("handicapped_infants", StringType()),
+    StructField("water_project_cost_sharing", StringType()),
+    StructField("adoption_of_the_budget_resolution", StringType()),
+    StructField("physician_fee_freeze", StringType()),
+    StructField("el_salvador_aid", StringType()),
+    StructField("religious_groups_in_schools", StringType()),
+    StructField("anti_satellite_test_ban", StringType()),
+    StructField("aid_to_nicaraguan_contras", StringType()),
+    StructField("mx_missile", StringType()),
+    StructField("immigration", StringType()),
+    StructField("synfuels_corporation_cutback", StringType()),
+    StructField("education_spending", StringType()),
+    StructField("superfund_right_to_sue", StringType()),
+    StructField("crime", StringType()),
+    StructField("duty_free_exports", StringType()),
+    StructField("export_administration_act_south_africa", StringType())
 ])
 
 print()
 
-data = spark.read.csv("./data.csv", header=True, schema=schema)
-data.show()
-data.registerTempTable("countries")
+data = spark.read.csv("./sample_data.csv", header=True, schema=schema)
+data.registerTempTable("voting_records")
 
-results = spark.sql("SELECT Capital FROM countries WHERE ID=3")
+results = spark.sql(
+    "SELECT * FROM voting_records WHERE mx_missile='n' AND adoption_of_the_budget_resolution='n' AND religious_groups_in_schools='?'")
 results.show()
