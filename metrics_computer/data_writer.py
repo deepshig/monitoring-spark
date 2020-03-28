8b65e9abd75733315fe39cfcc98bc83f03429f92
deepshig <deepshi1104@gmail.com>
2020-03-28 16:42:17 +0530

    Add idempotency for keyspace and table creation

--- metrics_computer/data_writer.py
+++ metrics_computer/data_writer.py
@@ -16,12 +16,12 @@ def shudown(cluster):
 
 
 def create_keyspace_and_tables(session):
-    create_keyspace_query = "CREATE KEYSPACE %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};" % KEYSPACE
+    create_keyspace_query = "CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};" % KEYSPACE
     session.execute(create_keyspace_query)
 
     session.set_keyspace(KEYSPACE)
 
-    create_table_query = "CREATE TABLE %s ( id uuid PRIMARY KEY, start_time float, time_taken float)" % DB_FETCH_TIME_TAKEN_TABLE
+    create_table_query = "CREATE TABLE IF NOT EXISTS %s ( id uuid PRIMARY KEY, start_time float, time_taken float)" % DB_FETCH_TIME_TAKEN_TABLE
     session.execute(create_table_query)
 
 
