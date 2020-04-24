d9dd5eecb02ec251fc89590d840555a14010f467
deepshig <deepshi1104@gmail.com>
2020-04-24 21:49:51 +0530

    Refactor batch processor

--- metrics_computer/batch_processor.py
+++ metrics_computer/batch_processor.py
@@ -2,6 +2,7 @@ import pandas as pd
 from statistics import median, stdev
 import sys
 import datetime
+import time
 sys.path.append('../')
 
 from metrics_computer.data_writer import create_session, create_keyspace_and_tables, get  # NOQA
@@ -10,7 +11,7 @@ from metrics_computer.data_writer import create_session, create_keyspace_and_tab
 def fetch_data(db_session):
     current_time = datetime.datetime.now()
 
-    query_time = current_time - datetime.timedelta(seconds=10)
+    query_time = current_time - datetime.timedelta(seconds=5)
     query_time_epoch = int(query_time.timestamp() * 1000)
 
     rows = get(db_session, query_time_epoch)
@@ -24,7 +25,7 @@ def process_data(rows):
         time_taken = row['time_taken']
         time_taken_list.append(time_taken)
 
-    if len(time_taken_list) != 0:
+    if len(time_taken_list) > 1:
         print("Median : ", median(time_taken_list))
         print("Standard Deviation : ", stdev(time_taken_list))
         print("++++++++++++")
@@ -36,5 +37,6 @@ def batch_processor():
     print("Batch Processor : Initialised Cassandra DB")
 
     while True:
+        time.sleep(3)
         rows = fetch_data(db_session)
         process_data(rows)
